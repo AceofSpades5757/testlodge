ccc0fe4cf3dcdeea77f607d8e7f3286c6ea19364
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-05-16 01:19:31 -0500

    Update user tests

---
+++ tests/user_test.py
@@ -0,0 +1,21 @@
+import unittest
+
+from testlodge import UserDetails
+
+
+class TestUser(unittest.TestCase):
+    """ Test User """
+
+    def test_simple(self):
+
+        user_dict: UserDetails = dict(
+            id=123456,
+            firstname='First',
+            lastname='Last',
+            email='user@email.com',
+            created_at="2022-01-01T20:30:40.123456Z",
+            updated_at="2022-05-16T01:08:41.493190Z",
+        )
+
+        # This operation is not supported at this time.
+        # self.assertTrue(isinstance(user_dict, UserDetails))
