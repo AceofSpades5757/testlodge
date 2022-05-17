19374d71a42a06bfab841dc7e7f17262fc01452c
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-05-16 20:22:45 -0500

    Fix deprecated test

--- tests/api_test.py
+++ tests/api_test.py
@@ -1,13 +1,8 @@
 import unittest
 
-from testlodge import SuiteSectionAPI
+from testlodge.api.suite_section import SuiteSectionAPI
 
 
 class TestAPI(unittest.TestCase):
-    def test_missing_client(self) -> None:
-        """API should not run without a client."""
-
-        api = SuiteSectionAPI()
-
-        with self.assertRaises(Exception):
-            api._list(1, 1)
+    def test_simple(self) -> None:
+        _ = SuiteSectionAPI('dummy-client')
