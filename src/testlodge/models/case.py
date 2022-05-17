2b8c855baf78858eb87e32b3278ca40a29e4e138
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-05-17 00:16:57 -0500

    Update Case model

--- src/testlodge/models/case.py
+++ src/testlodge/models/case.py
@@ -1,17 +1,20 @@
+from __future__ import __annotations__
+
 from datetime import datetime
 from typing import List
 from typing import TypedDict
 
 from testlodge._types import DateTimeStr
 from testlodge._types import Pagination
+from testlodge.client import Client
+from testlodge.models.custom_field import CustomField
 from testlodge.models.custom_field import CustomFieldJSON
-from testlodge.models.requirement_document import RequirementDocumentJSON
-from testlodge.models.user import UserJSON
 from testlodge.models.project import Project
+from testlodge.models.requirement_document import RequirementDocumentJSON
+from testlodge.models.requirements import RequirementDocument
 from testlodge.models.suite_section import SuiteSection
 from testlodge.models.user import User
-from testlodge.models.custom_field import CustomField
-from testlodge.models.requirements import RequirementDocument
+from testlodge.models.user import UserJSON
 
 
 class CaseJSON(TypedDict):
@@ -39,12 +42,11 @@ class CaseListJSON(TypedDict):
     steps: List[CaseJSON]
 
 
-class Case:
-    """ Represents a TestLodge test case. """
+class Case(BaseModel):
+    """Represents a TestLodge test case."""
 
     def __init__(
         self,
-
         id: int,
         step_number: str,
         title: str,
@@ -52,53 +54,97 @@ class Case:
         steps: str,
         expected_results: str,
         position: int,
-
         project: Project,
         suite_section: SuiteSection,
-
         last_saved_by: User,
-
         created_at: datetime,
         updated_at: datetime,
-
         custom_fields: List[CustomField],
         requirements: List[RequirementDocument],
-
+        client: Client = None,
     ):
         self._id = id
         self._step_number = step_number
-        self._title = title
-        self._description = description
-        self._steps = steps
-        self._expected_results = expected_results
+        self.title = title
+        self.description = description
+        self.steps = steps
+        self.expected_results = expected_results
         self._position = position
         self._project = project
         self._suite_section = suite_section
         self._last_saved_by = last_saved_by
         self._created_at = created_at
         self._updated_at = updated_at
-        self._custom_fields = custom_fields
+        self.custom_fields = custom_fields
         self._requirements = requirements
 
-    @classmethod
-    def from_json(cls, json: CaseJSON) -> Case:
-        id: int
-        project_id: int
-        suite_section_id: int
-        position: int
-        last_saved_by_id: int
-        last_saved_by: UserJSON
-        created_at: DateTimeStr
-        updated_at: DateTimeStr
-        custom_fields: List[CustomFieldJSON]
-        requirements: List[RequirementDocumentJSON]
-        step_number: str
-        title: str
-        description: str
-        test_steps: str
-        expected_result: str
+        self._client = client
 
-        project = Project.from_json(project_id)
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def step_number(self):
+        return self._step_number
+
+    @property
+    def position(self):
+        return self._position
+
+    @property
+    def project(self):
+        return self._project
+
+    @property
+    def suite_section(self):
+        return self._suite_section
+
+    @property
+    def last_saved_by(self):
+        return self._last_saved_by
+
+    @property
+    def created_at(self):
+        return self._created_at
+
+    @property
+    def updated_at(self):
+        return self._updated_at
+
+    @property
+    def requirements(self):
+        return self._requirements
+
+    @classmethod
+    def from_json(cls, json: CaseJSON, client: Client = None) -> Case:
+
+        # TODO
+        # custom_fields: List[CustomFieldJSON]
+        # TODO
+        # requirements: List[RequirementDocumentJSON]
+
+        # project = Project.from_json(id=project_id)
+        # suite_section = SuiteSection.from_json(id=suite_section_id)
+        project = LazyProject.from_json(id=project_id)
+        suite_section = LazySuiteSection.from_json(id=suite_section_id)
+        last_saved_by = User.from_json(last_saved_by)
+
+        created_at = cls.str_to_datetime(json['created_at'])
+        updated_at = cls.str_to_datetime(json['updated_at'])
 
         return cls(
             id=json['id'],
+            position=json['position'],
+            step_number=json['step_number'],
+            title=json['title'],
+            description=json['description'],
+            test_steps=json['test_steps'],
+            expected_result=json['expected_result'],
+            project=project,
+            suite_section=suite_section,
+            last_saved_by=last_saved_by,
+            created_at=created_at,
+            updated_at=updated_at,
+            client=client,
+        )
