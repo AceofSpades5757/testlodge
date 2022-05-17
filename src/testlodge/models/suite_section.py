cd3b7058fd69b61847341cbafeb649ddefa50e02
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-05-17 00:34:28 -0500

    Map out SuiteSection

--- src/testlodge/models/suite_section.py
+++ src/testlodge/models/suite_section.py
@@ -1,8 +1,14 @@
+from __future__ import __annotations__
+
 from typing import List
 from typing import TypedDict
+from typing import Union
 
 from testlodge._types import DateTimeStr
 from testlodge._types import Pagination
+
+
+AnySuiteSection = Union[SuiteSection, LazySuiteSection]
 
 
 class SuiteSectionJSON(TypedDict):
@@ -18,3 +24,22 @@ class SuiteSectionListJSON(TypedDict):
 
     pagination: Pagination
     suite_sections: List[SuiteSectionJSON]
+
+
+class SuiteSection:
+    # TODO
+    ...
+
+
+class LazySuiteSection:
+    """Create a suite section when needed, given a client."""
+
+    def __init__(self, id, client):
+
+        self.id = id
+        self.client = client
+
+    def gather(self) -> SuiteSection:
+        if self.client is None:
+            raise AttributeError("Client has not been set.")
+        return self.client.suite_section(self.id)
