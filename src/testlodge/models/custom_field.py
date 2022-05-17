e960526a23f8140db10b09b69e89bb277d417663
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-05-16 19:53:58 -0500

    Add custom field api

--- src/testlodge/models/custom_field.py
+++ src/testlodge/models/custom_field.py
@@ -1,3 +1,4 @@
+from typing import List
 from typing import TypedDict
 
 
@@ -6,3 +7,11 @@ class CustomFieldJSON(TypedDict):
     id: int
     name: str
     value: str
+
+
+class CustomFieldListJSON(TypedDict):
+
+    plan_content_custom_fields: List[CustomFieldJSON]
+    requirement_custom_fields: List[CustomFieldJSON]
+    step_custom_fields: List[CustomFieldJSON]
+    executed_step_custom_fields: List[CustomFieldJSON]
