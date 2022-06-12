be784ba4847af4dd4a7b0b750c26ebffb5d1bf4f
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-06-12 14:25:58 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fin:
 
 setuptools.setup(
     name='testlodge',
-    version='0.0.38',
+    version='0.0.39',
     author='Kyle L. Davis',
     author_email='AceofSpades5757.github@gmail.com',
     url='https://github.com/AceofSpades5757/testlodge',
